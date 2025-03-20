9c6c037c3af9e132701bcfad8e897ec50bde1288
Ameerah <ameeraharis03@gmail.com>
2025-03-20 12:15:58 +0400

    Fixed .gitignore issue

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
   server: {
     port: 3000 // Change this if needed
+  },
+  css: {
+    postcss: './postcss.config.js',
   }
-});
+})
